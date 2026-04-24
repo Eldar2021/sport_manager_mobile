161f1f23ce0ae0153e76ff89875c613328cdeac5
Duu1at <dbolsunbekuulu@gmail.com>
2026-04-24 08:37:51 +0600

    refactor: update home screen title and simplify body layout by removing unused dependencies and navigation button

--- app/lib/features/home/view/home_screen.dart
+++ app/lib/features/home/view/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:sport_manager_mobile/app/app.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,19 +8,11 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'test',
+          'home',
           style: Theme.of(context).textTheme.titleMedium,
         ),
       ),
-      body: Column(
-        children: [
-          ElevatedButton.icon(
-            icon: const Icon(Icons.settings_outlined),
-            label: const Text('test'),
-            onPressed: () => context.push(AppRoutes.settings),
-          ),
-        ],
-      ),
+      body: const Center(child: Text('home')),
     );
   }
 }
