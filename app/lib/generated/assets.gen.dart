990fe9407dd53c615eb47b1378882f20912b1fc2
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-04-27 14:44:47 +0600

    Update asset paths and add image assets for app branding

--- app/lib/generated/assets.gen.dart
+++ app/lib/generated/assets.gen.dart
@@ -20,6 +20,12 @@ class $AssetsIconsGen {
   /// File path: assets/icons/analytics.svg
   SvgGenImage get analytics => const SvgGenImage('assets/icons/analytics.svg');
 
+  /// File path: assets/icons/app_icon.svg
+  SvgGenImage get appIcon => const SvgGenImage('assets/icons/app_icon.svg');
+
+  /// File path: assets/icons/app_logo.svg
+  SvgGenImage get appLogo => const SvgGenImage('assets/icons/app_logo.svg');
+
   /// File path: assets/icons/call.svg
   SvgGenImage get call => const SvgGenImage('assets/icons/call.svg');
 
@@ -45,13 +51,118 @@ class $AssetsIconsGen {
   SvgGenImage get whatsapp02 => const SvgGenImage('assets/icons/whatsapp02.svg');
 
   /// List of all assets
-  List<SvgGenImage> get values => [analytics, call, email, home, profile, sms, telegram, whatsapp, whatsapp02];
+  List<SvgGenImage> get values => [
+    analytics,
+    appIcon,
+    appLogo,
+    call,
+    email,
+    home,
+    profile,
+    sms,
+    telegram,
+    whatsapp,
+    whatsapp02,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/app_icon.png
+  AssetGenImage get appIcon => const AssetGenImage('assets/images/app_icon.png');
+
+  /// File path: assets/images/app_logo.png
+  AssetGenImage get appLogo => const AssetGenImage('assets/images/app_logo.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [appIcon, appLogo];
 }
 
 class Assets {
   const Assets._();
 
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}, this.animation});
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({required this.isAnimation, required this.duration, required this.frames});
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
 }
 
 class SvgGenImage {
