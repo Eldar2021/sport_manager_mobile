7177c5888a12b376864c1d3a4a40d2fdc143ea33
Duu1at <dbolsunbekuulu@gmail.com>
2026-04-27 14:02:45 +0600

    refactor: consolidate and simplify component exports in components.dart

--- app/lib/ui/components/components.dart
+++ app/lib/ui/components/components.dart
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-export 'banner/info_banner.dart';
-export 'body/app_destructive_sheet.dart';
-export 'button/app_delete_button.dart';
-export 'button/app_submit_button.dart';
-export 'contact/contact_support_sheet.dart';
-=======
 export 'banner/app_banner.dart';
 export 'button/app_button.dart';
 export 'button/app_button_scope.dart';
->>>>>>> 8c6d2f594c2652b7a89a149cace94f0590e62b70
 export 'form/app_checkbox_field.dart';
 export 'form/app_password_field.dart';
 export 'form/app_text_field.dart';
