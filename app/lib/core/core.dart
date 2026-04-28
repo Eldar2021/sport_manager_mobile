5c08cbce1cdda09f003cc9d5bf57e642fd885b74
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-04-28 16:10:25 +0600

    refactor: replace MainShell with MainView and update routing in app_router.dart

--- app/lib/core/core.dart
+++ app/lib/core/core.dart
@@ -2,7 +2,6 @@ export 'bloc/bloc_cancel_token.dart';
 export 'di/di.dart';
 export 'exeptions/exeption.dart';
 export 'exeptions/extension/error_handler_context_extension.dart';
-export 'shell/main_shell.dart';
 export 'state/data_state.dart';
 export 'utils/auth_snackbar.dart';
 export 'utils/input_validators.dart';
