13c695820d68a338b19e19b029f71f1da0d64866
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-04-28 16:36:38 +0600

    refactor: enhance HomeView with floating action button and navigation to table form

--- app/lib/features/home/view/home_view.dart
+++ app/lib/features/home/view/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sport_manager_mobile/app/app.dart';
+import 'package:sport_manager_mobile/features/tables/tables.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,6 +9,17 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.push(
+            AppRoutes.tableForm,
+            extra: const TableFormExtra(
+              venueId: 'venue_id',
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text('Home'),
       ),
