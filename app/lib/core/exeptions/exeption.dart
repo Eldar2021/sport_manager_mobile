e5812fb5a1a88d0a7af5c86e8f32f1b59cdde56a
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-04-29 13:32:41 +0600

    refactor: move currency and tarif type extensions to their own files

--- app/lib/core/exeptions/exeption.dart
+++ app/lib/core/exeptions/exeption.dart
@@ -1,6 +1,4 @@
-export 'extension/currency_extension.dart';
 export 'extension/error_handler_context_extension.dart';
-export 'extension/tarif_type_extension.dart';
 export 'handle/app_exception_type_resolver.dart';
 export 'handle/unauthenticated_exception_handle.dart';
 export 'widget/error_body_widget.dart';
