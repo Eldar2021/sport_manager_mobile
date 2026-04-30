0d91fa52b8d5f24bebeab3f75ff277ba73211f31
Eldar2021 <eldiiaralmazbekov@gmail.com>
2026-05-01 00:09:54 +0600

    feat(subscription): update exports in checkout and detail modules for improved structure

--- app/lib/features/subscription/checkout/checkout.dart
+++ app/lib/features/subscription/checkout/checkout.dart
@@ -1,2 +1,5 @@
 export 'cubit/subscription_checkout_cubit.dart';
 export 'view/subscription_checkout_view.dart';
+export 'widgets/subscription_checkout_content.dart';
+export 'widgets/subscription_no_tables.dart';
+export 'widgets/subscription_pay_fab.dart';
--- app/lib/features/subscription/detail/detail.dart
+++ app/lib/features/subscription/detail/detail.dart
@@ -1,2 +1,6 @@
 export 'cubit/subscription_detail_cubit.dart';
 export 'view/subscription_view.dart';
+export 'widgets/subscription_continue_fab.dart';
+export 'widgets/subscription_detail_content.dart';
+export 'widgets/subscription_error_view.dart';
+export 'widgets/subscription_skeleton.dart';
